fda2685de1b2b7ea72113e24b1ca8a7fe1bc9d30
luisda190519 <licerol@uninorte.edu.co>
2023-03-20 13:47:10 -0500

    Update Notifications.jsx

--- src/Views/Notifications.jsx
+++ src/Views/Notifications.jsx
@@ -1,5 +1,22 @@
-function Notifications(){
-    return <h1>Notif</h1>
+function Notifications() {
+    return (
+        <div>
+            <h2>Mis alertas</h2>
+            <div className="row">
+                <div className="col-11 card mt-4 text-secondary">
+                    <div className="mt-3 ms-3">
+                        <h4 className="card-title my-0">
+                            <i className="bi bi-bell me-2"></i>Mis alertas
+                        </h4>
+                        <p className="">
+                            Reciba por e-mail las ofertas que más te interesan
+                            en base a los criterios que defina en tus alertas.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Notifications;
